4756a7d37f82db9f7467fd1aee23d87213fe97b4
ecamo19 <erickcamo19@gmail.com>
2022-07-25 16:39:59 +0000

    file ensemble.samples.NOENSEMBLEID.Rdata has no id

---
+++ scripts/ensemble_run.R
@@ -0,0 +1,25 @@
+# Objective --------------------------------------------------------------------
+# This script was created for running the ensemble.R and run.ensemble.analysis.R
+# functions
+
+# Load functions ---------------------------------------------------------------
+source("~/gsoc_project_2022/R/ensemble.R")
+source("~/gsoc_project_2022/R/run.ensemble.analysis.R")
+
+
+# Change working directory to pecan run -----------------------------------
+setwd('~/gsoc_project_2022/pecan_runs/run_2022-07-25/')
+dir()
+
+# Load packages ----------------------------------------------------------------
+library(PEcAn.all)
+library(devtools)
+
+# Load xml file  ---------------------------------------------------------------
+#settings <- read.settings(".xml")
+
+# Run --------------------------------------------------------------------------
+
+
+
+
