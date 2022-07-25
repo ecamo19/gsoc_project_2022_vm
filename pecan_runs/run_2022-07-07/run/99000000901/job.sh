50010d7257da3976420aadfdecbe9a2c7ac39b95
ecamo19 <erickcamo19@gmail.com>
2022-07-25 15:13:41 +0000

    older folder deleted

--- pecan_runs/run_2022-07-07/run/99000000901/job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p /home/carya/./gsoc_project_2022/pecan_runs/run_2022-07-07/out/99000000901
-cd /home/carya/./gsoc_project_2022/pecan_runs/run_2022-07-07/run/99000000901
- /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-07/run/99000000901 /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-07/out/99000000901
-if [ $? -ne 0 ]; then
-    echo ERROR IN MODEL RUN >&2
-    exit 1
-fi
-cp  /home/carya/./gsoc_project_2022/pecan_runs/run_2022-07-07/run/99000000901/README.txt /home/carya/./gsoc_project_2022/pecan_runs/run_2022-07-07/out/99000000901/README.txt
