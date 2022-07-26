b93e420ed5ec1d8bfcc29dcc32555a06f9163a8f
ecamo19 <erickcamo19@gmail.com>
2022-07-26 16:55:49 +0000

    files deleted

--- pecan_runs/run_2022-07-25/run/SA-salix-growth_respiration_coefficient-0.159/job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-25/out/SA-salix-growth_respiration_coefficient-0.159
-cd /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-25/run/SA-salix-growth_respiration_coefficient-0.159
-~/gsoc_project_2022/models/biocro.Rscript /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-25/run/SA-salix-growth_respiration_coefficient-0.159 /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-25/out/SA-salix-growth_respiration_coefficient-0.159
-if [ $? -ne 0 ]; then
-    echo ERROR IN MODEL RUN >&2
-    exit 1
-fi
-cp  /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-25/run/SA-salix-growth_respiration_coefficient-0.159/README.txt /home/carya/gsoc_project_2022/pecan_runs/run_2022-07-25/out/SA-salix-growth_respiration_coefficient-0.159/README.txt
