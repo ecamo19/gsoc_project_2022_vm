a94af23c31028417b4889d5ef479abf49e2743c0
ecamo19 <erickcamo19@gmail.com>
2022-08-05 22:29:11 +0000

    Code updated, no major changes[A

--- scripts/basic_run.R
+++ scripts/basic_run.R
@@ -33,7 +33,7 @@ settings <- PEcAn.settings::read.settings("./gsoc_project_2022/xml_files/simple_
 # Configure settings -----------------------------------------------------------
 
 # Get date
-path <- paste0('gsoc_project_2022/pecan_runs/pecan_run_salix')
+path <- paste0('./gsoc_project_2022/pecan_runs/pecan_run_salix')
 
 # Set output dir
 settings$outdir <- file.path(path)
